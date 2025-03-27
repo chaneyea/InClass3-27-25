ac8c9ca06aa68ef407b48634e27b78af965eaaae
Ethan Chaney <chaneyea@mail.uc.edu>
2025-03-27 11:58:44 -0400

    Add project files.

---
+++ InClass3-27-25/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass20250311
+{
+    internal class Program
+    {
+        static void Main(string[] args) // This is the 'entry point' of our project
+        {
+            // Console.WriteLine("Hello");
+            int result;
+            result = Cube(42); // "Invoke or Call" the method
+            Console.WriteLine("42 cubed is " + result);
+        }
+        /// <summary>
+        /// Compute the cube of an integer
+        /// </summary>
+        /// <param name="num">The number to be cubed</param>
+        /// <returns>The cube of num</returns>
+        static int Cube(int num)  // The method 'signature'
+        {
+            return num * num * num;
+        }
+    }
+}
